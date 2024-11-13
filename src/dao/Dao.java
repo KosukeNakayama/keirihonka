434a3ae0fc52f50374ee15840f800d430f976431
ku_nakayama <ku_nakayama@host.docker.internal>
2024-11-13 15:44:54 +0900

    DB接続用

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,9 +1,14 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import bean.Student;
 
 public class Dao {
 	/**
@@ -20,12 +25,62 @@ public class Dao {
 	public Connection getConnection() throws Exception {
 		// データソースがnullの場合
 		if (ds == null) {
-			// InitialContextを初期化
-			InitialContext ic = new InitialContext();
-			// データベースへ接続
-			ds = (DataSource) ic.lookup("java:/comp/env/jdbc/hayashi");
+			InitialContext ic=new InitialContext();
+			DataSource ds=(DataSource)ic.lookup("java:/comp/env/jdbc/keirihonka");
 		}
 		// データベースへのコネクションを返却
 		return ds.getConnection();
+	}
+
+	/**
+	 * test用メソッド
+	 * DBと接続できてるかのテスト
+	 */
+	public void test() throws Exception {
+			// 学校インスタンスを初期化
+			Student stu = new Student();
+			// データベースへのコネクションを確率
+			Connection connection = getConnection();
+			// プリペアードステートメント
+			PreparedStatement statement = null;
+
+			try {
+				// プリペアードステートメントにSQL文をセット
+				statement = connection.prepareStatement("select * from student");
+				// プリペアードステートメントを実行
+				ResultSet rSet = statement.executeQuery();
+
+				if (rSet.next()) {
+					// リザルトセットが存在する場合
+					// 学校インスタンスに学校コードと学校名をセット
+					stu.setStudentId(rSet.getString("student_id"));
+					stu.setStudentName(rSet.getString("student_name"));
+
+				} else {
+					// 存在しない場合
+					// 学校インスタンスにnullをセット
+					stu = null;
+				}
+			} catch (Exception e) {
+				throw e;
+			} finally {
+				// プリペアードステートメントを閉じる
+				if (statement != null) {
+					try {
+						statement.close();
+					} catch (SQLException sqle) {
+						throw sqle;
+					}
+				}
+				// コネクションを閉じる
+				if (connection != null) {
+					try {
+						connection.close();
+					} catch (SQLException sqle) {
+						throw sqle;
+					}
+				}
+			}
+			System.out.println(stu);
 	}
 }
--- src/servlet/Sample.java
+++ src/servlet/Sample.java
@@ -1,5 +1,26 @@
 package servlet;
 
-public class Sample {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.Dao;
+
+
+@WebServlet(urlPatterns={"/servlet/sample"})
+public class Sample extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Dao dao = new Dao();
+		try {
+			dao.test();
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+    }
 }
+
