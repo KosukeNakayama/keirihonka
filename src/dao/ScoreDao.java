5e5332721a0d75c2b30ae7514a702f2486630a88
tamori <tamori@192.168.10.108>
2025-02-24 14:15:39 +0900

    田森成績確認

--- src/dao/ScoreDao.java
+++ src/dao/ScoreDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +17,30 @@ public class ScoreDao extends Dao{
 
 		return list;
 	}
-
-
+	//学生で検索
+	public List<Score> searchStudent(String student_id) throws Exception{
+		Connection con=getConnection();
+		List<Score> list =new ArrayList<>();
+		PreparedStatement st=con.prepareStatement(
+				"select score.student_id,student_name,subject_name,score,year,month from score join subject on score.subject_id = subject.subject_id"
+				+ " join student on score.student_id = student.student_id "
+				+ " where score.student_id= ? ");
+		st.setString(1, student_id);
+		ResultSet rs=st.executeQuery();
+		while (rs.next()) {
+			Score p=new Score();
+			p.setStudentId(rs.getString("student_id"));
+			p.setStudentName(rs.getString("student_name"));
+			p.setSubjectName(rs.getString("subject_name"));
+			p.setScore(rs.getInt("score"));
+			p.setYear(rs.getInt("year"));
+			p.setMonth(rs.getInt("month"));
+			list.add(p);
+		}
+		st.close();
+		con.close();
+		return list;
+	}
 	//学生登録
 	public int insert(Score score) throws Exception {
 		Connection con=getConnection();
